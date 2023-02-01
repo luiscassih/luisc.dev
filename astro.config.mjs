b0366ebba8eec21153a0d3d96e004a60ef9db082
Luis C <luiscassih@gmail.com>
2023-02-01 14:14:29 +0100

    basic astro template

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [tailwind()]
+});
