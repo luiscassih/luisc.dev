7317e0daeaf9cdfea55736dc3c965115cf5a686b
Luis C <luiscassih@gmail.com>
2023-03-06 17:06:59 +0100

    added images - github workflow pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://laincode.com',
   integrations: [tailwind()]
 });
